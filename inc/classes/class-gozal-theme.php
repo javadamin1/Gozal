796f09af7987f9552f74e6e6b3347092a999f8f7
javad <javadamin93@gmail.com>
2022-01-26 22:19:46 +0330

    add logo style

--- inc/classes/class-gozal-theme.php
+++ inc/classes/class-gozal-theme.php
@@ -21,7 +21,16 @@ protected function __construct(){
         /**
          * Action.
          */
-        add_action(');
+        add_action('after_setup_theme',[$this , 'setup_theme'] );
+    }
+    public function setup_theme(){
+    add_theme_support('title-tag');
+    add_theme_support('custom-logo',[
+        'height' =>200,
+        'width' =>200,
+        'flex-height' => false ,
+        'flex-width' => false ,
+    ]);
     }
 
 }
