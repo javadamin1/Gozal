09a8ff42b9e5b816bdf1816f3a404d16ceada354
javad <javadamin93@gmail.com>
2022-01-27 22:39:41 +0330

    fix header menu

--- inc/classes/class-menus.php
+++ inc/classes/class-menus.php
@@ -36,7 +36,8 @@ class Menus{
             'gozal-footer-menu' => esc_html__( 'Footer Menu', 'gozal' ),
         ]);
     }
-    public function get_menu_id($location){
+    public function get_menu_id ($location): int
+    {
         //Get all the locations.
 
         $locations=get_nav_menu_locations();
@@ -44,6 +45,18 @@ class Menus{
         $menu_id = $locations[$location];
 
         return ! empty($menu_id) ? $menu_id : '';
+    }
+    public  function get_child_menu_items( $menu_array , $Parent_id ): array
+    {
+        $child_menus=[];
+        if(!empty($menu_array) && is_array($menu_array)){
+            foreach ($menu_array as $menu ) {
+                if(intval($menu->menu_item_parent)===$Parent_id){
+                  $child_menus[] = $menu;
+                }
+            }
+        }
+        return $child_menus;
     }
 
 
--- template-parts/header/nav.php
+++ template-parts/header/nav.php
@@ -8,67 +8,74 @@
 
 $menu_class=\GOZAL_THEME\Inc\Menus::get_instance();
 $header_menu_id = $menu_class -> get_menu_id('gozal-header-menu');
-$header_menu = wp_get_nav_menu_items($header_menu_id);
-echo '<pre>';
-print_r($header_menu);
-wp_die();
+$header_menus = wp_get_nav_menu_items($header_menu_id);
+
 
 ?>
-<!-- <nav class="navbar navbar-expand-lg  navbar-dark bg-primary">
-    <div class="container" >
 
- 
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div class="navbar-nav">
-      <a class="nav-item nav-link active" href="#">خانه <span class="sr-only">(current)</span></a>
-      <a class="nav-item nav-link" href="#">خدمات</a>
-      <a class="nav-item nav-link" href="#">تماس با ما</a>
-      <a class="nav-item nav-link" href="#">درباره ما</a>
-       <a class="nav-item nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a> 
-    </div>
-  </div>
-   <a class="navbar-brand" href="#">Navbar</a>
-</div>
-</nav> -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary ">
     <div class="container">
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
   <div class="collapse navbar-collapse justify-content-between" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">خانه <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">خدمات</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">تماس با ما</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">درباره ما</a>
-      </li>
-     
-      <!-- <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li> -->
-      <!-- <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li> -->
-    </ul>
+      <?php
+      if( ! empty($header_menus) && is_array($header_menus) ){
+        ?>
+      <ul class="navbar-nav mr-auto">
+        <?php
+        foreach ($header_menus as $menu_item){
+            if(! $menu_item -> menu_item_parent ){
+                $child_menu_items = $menu_class->get_child_menu_items( $header_menus , $menu_item->ID);
+            $has_children =! empty( $child_menu_items ) && is_array($child_menu_items);
+            if ( !$has_children){
+                ?>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo esc_url( $menu_item -> url )?>"> <?php
+                        echo esc_html($menu_item -> title)
+                        ?> </a>
+                </li>
+          <?php
+            }else{
+                ?>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="<?php echo esc_url($menu_item -> url) ?>"
+                       id="navbarDropdown" role="button"
+                       data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <?php echo esc_html($menu_item -> title) ?>
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <?php
+                        foreach ( $child_menu_items as $child_item){
+                            ?>
+                            <a class="dropdown-item" href=" <?php echo esc_url($child_item -> url) ?>"> <?php echo
+                                esc_html($child_item -> title) ?></a>
+                            <?php
+                        }
+                        ?>
+
+
+                    </div>
+                </li>
+                <?php
+
+            }
+            }
+        }
+
+        ?>
+
+
+
+
+
+      </ul>
+      <?php
+      }
+      ?>
+
     <a class="navbar-brand" href="#">
         <?php if ( function_exists( 'the_custom_logo' ) ) {
 		the_custom_logo();
