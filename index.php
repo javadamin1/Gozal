c45e3dd6ce9e1745462ab342d70f91c9970f160d
javad <javadamin93@gmail.com>
2022-02-01 21:40:57 +0330

    add title to page

--- index.php
+++ index.php
@@ -5,11 +5,39 @@
  */
 get_header();
 ?>
+<div id="primary">
+  <main id="main" class="site-main mt-5" role="main" >
+      <?php
+      if(have_posts()){
 
-    <div class="container">
-<div style="width: 200px;height:100px  "  class="btn btn-success" >continue</div>
+          ?>
+          <div class="container">
+              <?php
+              if(is_home() && ! is_front_page()){
+                  ?>
+                  <header class="mb-5" >
+                      <h1 class="page-title screen-leader-text" >
+                          <?php
+                          single_post_title();
+                          ?>
 
-    </div>
+                      </h1>
+                  </header>
+                      <?php
+              }
+              while (have_posts()) : the_post();
+              the_title( );
+              the_content();
+      endwhile;
+              ?>
+          </div>
+          <?php
+      }
+      ?>
+  </main>
+
+</div>
+
 <?php
 get_footer();
 
