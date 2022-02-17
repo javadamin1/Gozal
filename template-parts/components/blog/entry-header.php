288b8de0c13f52ac478f64b0a0442c29b1b8d329
javad <javadamin93@gmail.com>
2022-02-17 16:41:28 +0330

    blog

---
+++ template-parts/components/blog/entry-header.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * Template for post entry header
+ *
+ * @package gozal
+ */
--- template-parts/content.php
+++ template-parts/content.php
@@ -7,7 +7,15 @@
 */
 ?>
 
-<h3><?php the_title(); ?></h3>
-<div> <?php the_excerpt(); ?>  </div>
-<h4><?php the_time('Y-m-d H:i:s'); ?></h4>
-<h4> نویسنده:<?php the_author(); ?></h4>
+<!--<h3>--><?php //the_title(); ?><!--</h3>-->
+<!--<div> --><?php //the_excerpt(); ?><!--  </div>-->
+<!--<h4>--><?php //the_time('Y-m-d H:i:s'); ?><!--</h4>-->
+<!--<h4> نویسنده:--><?php //the_author(); ?><!--</h4>-->
+<article id="post-<?php the_ID(); ?>" <?php post_class('mb-5'); ?> >
+    <?php
+    get_template_part('template-parts/component/blog/entry-header');
+    get_template_part('template-parts/component/blog/entry-meta');
+    get_template_part('template-parts/component/blog/entry-content');
+    get_template_part('template-parts/component/blog/entry-footer');
+    ?>
+</article>
