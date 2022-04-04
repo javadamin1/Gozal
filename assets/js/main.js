8212c0e0c51f244ad0686ae9dfe41ae2544f28ab
javadamin1 <javadamin93@gmail.com>
2022-04-04 23:19:53 +0430

    design blog page and optimiz

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,41 @@
 jQuery(function ($) {
-    $(".hamburger__button").click(function(){
-        $(this).toggleClass("toggled");
-        $(".menu").toggleClass("menu-drop");
-        $("section").toggleClass("blur");
-      });
-      $("section").click(function(){
-        $(".hamburger__button").removeClass("toggled");
-        $(".menu").removeClass("menu-drop");
-        $(this).removeClass("blur");
-      });
+  //drop menu
+  $(".hamburger__button").click(function () {
+    $(this).toggleClass("toggled");
+    $(".menu").toggleClass("menu-drop");
+    $("section").toggleClass("blur");
+  });
+  // remove menu and blur onclick section
+  $("section").click(function () {
+    $(".hamburger__button").removeClass("toggled");
+    $(".menu").removeClass("menu-drop");
+    $(this).removeClass("blur");
+  });
 
+  //ajax load more post botton
+  $(".load-more-btn").on("click", function () {
 
-
+    var that = $(this);
+    var page = that.data("page");
+    var newpage = page+1;
+    var ajax_url = that.data("url");
+console.
+    $.ajax({
+      url: ajax_url,
+      type : 'post',
+      data: {
+        page: page,
+        action: "gozal_load_more",
+      },
+      error: function (response) {
+        console.log('de'+response);
+      },
+      success: function (response) {
+        
+        console.log('su'+response);
+        $(".load").append(response);
+that.data('page',newpage);
+      },
+    });
+  });
 });
